8abecca9092ebe71b7edef73d6a4deb480af8047
Halit Aksoy <jhalitaksoy@gmail.com>
2022-05-08 16:59:35 +0300

    add GameStep

--- src/core/HistoryItem.ts
+++ src/core/HistoryItem.ts
@@ -1,16 +1,34 @@
 export class HistoryItem {
   boardSnapshot: number[];
-  constructor(boardSnapshot: number[]) {
+  gameSteps: GameStep[];
+  constructor(boardSnapshot: number[], gameSteps: GameStep[]) {
     this.boardSnapshot = boardSnapshot;
+    this.gameSteps = gameSteps;
   }
 }
 
 export class MoveHistoryItem extends HistoryItem {
   playerId: string;
   moveIndex: number;
-  constructor(playerId: string, moveIndex: number, boardSnapshot: number[]) {
-    super(boardSnapshot);
+  constructor(
+    playerId: string,
+    moveIndex: number,
+    boardSnapshot: number[],
+    gameSteps: GameStep[]
+  ) {
+    super(boardSnapshot, gameSteps);
     this.playerId = playerId;
     this.moveIndex = moveIndex;
+  }
+}
+
+export class GameStep {
+  index: number;
+  type: string;
+  data: any | null;
+  constructor(index: number, type: string, data: any = null) {
+    this.index = index;
+    this.type = type;
+    this.data = data;
   }
 }
--- tests/MancalaGame.test.ts
+++ tests/MancalaGame.test.ts
@@ -102,27 +102,55 @@ describe('Game Test', () => {
     game.moveByPlayerPit(player2Id, 0);
     game.moveByPlayerPit(player1Id, 1);
     game.moveByPlayerPit(player2Id, 1);
-    expect(game.history).toStrictEqual([
-      new MoveHistoryItem(
-        player1Id,
-        0,
-        [1, 5, 5, 5, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
-      ),
-      new MoveHistoryItem(
-        player2Id,
-        7,
-        [1, 5, 5, 5, 4, 4, 0, 1, 5, 5, 5, 4, 4, 0]
-      ),
-      new MoveHistoryItem(
-        player1Id,
-        1,
-        [1, 1, 6, 6, 5, 5, 0, 1, 5, 5, 5, 4, 4, 0]
-      ),
-      new MoveHistoryItem(
-        player2Id,
-        8,
-        [1, 1, 6, 6, 5, 5, 0, 1, 1, 6, 6, 5, 5, 0]
-      )
-    ]);
+    const historyItem1 = new MoveHistoryItem(
+      player1Id,
+      0,
+      [1, 5, 5, 5, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0],
+      [
+        new GameStep(0, GAME_STEP_GAME_MOVE),
+        new GameStep(1, GAME_STEP_GAME_MOVE),
+        new GameStep(2, GAME_STEP_GAME_MOVE),
+        new GameStep(3, GAME_STEP_GAME_MOVE)
+      ]
+    );
+    const historyItem2 = new MoveHistoryItem(
+      player2Id,
+      7,
+      [1, 5, 5, 5, 4, 4, 0, 1, 5, 5, 5, 4, 4, 0],
+      [
+        new GameStep(7, GAME_STEP_GAME_MOVE),
+        new GameStep(8, GAME_STEP_GAME_MOVE),
+        new GameStep(9, GAME_STEP_GAME_MOVE),
+        new GameStep(10, GAME_STEP_GAME_MOVE)
+      ]
+    );
+    const historyItem3 = new MoveHistoryItem(
+      player1Id,
+      1,
+      [1, 1, 6, 6, 5, 5, 0, 1, 5, 5, 5, 4, 4, 0],
+      [
+        new GameStep(1, GAME_STEP_GAME_MOVE),
+        new GameStep(2, GAME_STEP_GAME_MOVE),
+        new GameStep(3, GAME_STEP_GAME_MOVE),
+        new GameStep(4, GAME_STEP_GAME_MOVE),
+        new GameStep(5, GAME_STEP_GAME_MOVE)
+      ]
+    );
+    const historyItem4 = new MoveHistoryItem(
+      player2Id,
+      8,
+      [1, 1, 6, 6, 5, 5, 0, 1, 1, 6, 6, 5, 5, 0],
+      [
+        new GameStep(8, GAME_STEP_GAME_MOVE),
+        new GameStep(9, GAME_STEP_GAME_MOVE),
+        new GameStep(10, GAME_STEP_GAME_MOVE),
+        new GameStep(11, GAME_STEP_GAME_MOVE),
+        new GameStep(12, GAME_STEP_GAME_MOVE)
+      ]
+    );
+    expect(game.history[0]).toStrictEqual(historyItem1);
+    expect(game.history[1]).toStrictEqual(historyItem2);
+    expect(game.history[2]).toStrictEqual(historyItem3);
+    expect(game.history[3]).toStrictEqual(historyItem4);
   });
 });
